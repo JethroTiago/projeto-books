3f39bcb08750d1551399a35f43a719b0588ee543
Jethro <jethrotiago@hotmail.com>
2023-02-02 14:06:21 -0300

    Deletado const que bugava filter

--- app/filter.js
+++ app/filter.js
@@ -1,5 +1,4 @@
-const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
-btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivros);
+
 
 function filtrarLivros() {
     let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end');
